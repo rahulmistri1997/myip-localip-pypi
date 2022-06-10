8448d4a3139b3bca6ef1de4270e8eb5c23318713
Rahul Mistri <58909377+rahulmistri1997@users.noreply.github.com>
2022-06-10 17:02:22 +0000

    update: Handling long descirption error from pypy

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	name="myip_localip",
 	version="0.0.1",
-	description="This uses ipinfo.io to gather data about current ip .Also give LocalIP in the Response",
+	description="This uses ipinfo.io about current ip Also give LocalIP",
 	install_requires=["click==8.1.3", "colorama==0.4.4", "requests"],
 	entry_points="""
 	[console_scripts]
