d896dbec80e5f3d35e29b3a116caaf2433544e1b
Rahul Mistri <58909377+rahulmistri1997@users.noreply.github.com>
2022-06-10 17:18:28 +0000

    fix: Release

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
 	name="myip_localip",
 	version="0.0.1",
 	description="This uses ipinfo.io about current ip Also give LocalIP",
+	long_description="This uses ipinfo.io about current ip Also give LocalIP",
 	install_requires=["click==8.1.3", "colorama==0.4.4", "requests"],
 	entry_points="""
 	[console_scripts]
