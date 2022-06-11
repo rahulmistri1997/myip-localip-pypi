78ee9dd59eaec9feb837496f976bafbac605c859
Rahul Mistri <58909377+rahulmistri1997@users.noreply.github.com>
2022-06-11 13:19:24 +0000

    update: Bump to V0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="myip_localip",
-	version="0.0.1",
+	version="0.0.7",
 	description="This uses ipinfo.io about current ip Also give LocalIP",
 	long_description="This uses ipinfo.io about current ip Also give LocalIP",
 	install_requires=["click==8.1.3", "colorama==0.4.4", "requests"],
